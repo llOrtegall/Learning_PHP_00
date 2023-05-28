67f884d039e57af4ee4ef554e7dcc4bc0f922901
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-05-28 13:35:29 -0500

    agregando schema

---
+++ schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE notes (
+    id int NOTT NULL AUTO_INCREMENT,
+    uuid varchar(255) NOT NULL UNIQUE,
+    title varchar(255) NOT NULL,
+    content text NOT NULL,
+    updated date NOT NULL,
+    PRIMARY KEY (id)
+)
