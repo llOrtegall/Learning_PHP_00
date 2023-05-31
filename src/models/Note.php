a651f2ff71d278a9fb2c8ec46d5f17d815814db8
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-05-31 14:12:37 -0500

    notes.php agregando funciones

---
+++ src/models/Note.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Proyect\Notas\models;
+
+use Proyect\Notas\lib\DataBase;
+
+class Note extends DataBase
+{
+
+    private string $uuid;
+
+    public function __construct(private string $title, private string $content)
+    {
+        parent::__construct();
+
+        $this->uuid = uniqid();
+    }
+
+    public function save()
+    {
+        $query = $this->connect()->prepare("INSERT INTO notes(uuid, title, content, updated) VALUES(:uuid, :title, :content, NOW()");
+        $query->execute(['title' => $this->title, 'uuid' => $this->uuid, 'content' => $this->content]);
+    }
+
+    public function getUUID()
+    {
+        return $this->uuid;
+    }
+
+    public function setUUID($value)
+    {
+        $this->uuid = $value;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle($value)
+    {
+        $this->title = $value;
+    }
+
+    public function getContent()
+    {
+        return $this->content;
+    }
+    public function setContent($value)
+    {
+        $this->content = $value;
+    }
+}
