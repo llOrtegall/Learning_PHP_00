35f4e29e43ca1fdf6754093db37836aff5a13182
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-05-31 14:01:52 -0500

    databse create ok

---
+++ src/lib/Database.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Proyect\Notas\lib;
+
+use PDO;
+use PDOException;
+
+class DataBase
+{
+    private string $host;
+    private string $db;
+    private string $user;
+    private string $password;
+    private string $charset;
+
+    public function __construct()
+    {
+        $this->host = 'localhost';
+        $this->db = 'notes';
+        $this->user = 'root';
+        $this->password = '';
+        $this->charset = 'utf8mb4';
+    }
+
+    public function connect()
+    {
+        try {
+            $connection = "mysql:host={$this->host};dbname={$this->db};charset={$this->charset}";
+            $options = [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                pdo::ATTR_EMULATE_PREPARES => false
+            ];
+
+            $pdo = new PDO($connection, $this->user, $this->password, $options);
+            return $pdo;
+        } catch (PDOException $th) {
+            throw $th;
+        }
+    }
+}
