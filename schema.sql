e0eda654e700984b97e041ca9a80b09f947b1515
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-06-15 12:58:56 -0500

    desde 0 con tailwind css via cdn

--- schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE notes (
-    id int NOTT NULL AUTO_INCREMENT,
-    uuid varchar(255) NOT NULL UNIQUE,
-    title varchar(255) NOT NULL,
-    content text NOT NULL,
-    updated date NOT NULL,
-    PRIMARY KEY (id)
-);
--- src/app.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (isset($_GET['view'])) {
-    $view = $_GET['view'];
-    require 'src/views/' . $view . '.php';
-} else {
-    require 'src/views/home.php';
-}
