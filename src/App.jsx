9ce156abddf259324123fb6fed883eecf5df9688
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-12-02 09:05:20 -0500

    agregar un layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { Route, Routes } from 'react-router-dom'
+
+import { NavBar } from './components/NavBar'
+
+import { Home } from './pages/Home'
+import { About } from './pages/About'
+import { Products } from './pages/Products'
+
 export function App () {
   return (
-    <h1>
-      Hello World!
-    </h1>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='products' element={<Products />} />
+      </Routes>
+    </>
   )
 }
